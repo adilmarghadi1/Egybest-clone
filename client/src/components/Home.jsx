d1a160d09772b885b1bf9a505ed0f571f5bdfa76
adilmarghadi1 <adilmarghadi2020@gmail.com>
2022-03-24 00:36:16 +0100

    Add

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
     return (
         <div className="container1">
             <div className='left'>
+                <div className='box11'> 
                 <div className='box1'>
                     <h1>تابع ايجي بست</h1>
                     <div className='links'>
@@ -17,6 +18,31 @@ function Home() {
                     <FaTwitterSquare className='linksf'/>
                     </div>
                 </div>
+
+                <div className='box2'>
+                    <div className='link2'>
+                        <a href='#'>افلام 2021</a>
+                  
+                        <a href='#'>افلام عربية</a>
+                    </div>
+
+                    <div className='link2'>
+                        <a href='#'>افلام HD</a>
+              
+                        <a href='#'>افلام بلوراى</a>
+                    </div>
+
+
+                    <div className='link2'>
+                        <a href='#'>افلام مصرية</a>
+                
+                        <a href='#'>افلام كورية</a>
+                    </div>
+
+
+                  
+                </div>
+            </div>
             </div>
         </div>
     )
