edeab69d96ce28c1aa114af2710416dc3dd0d70f
adilmarghadi1 <adilmarghadi2020@gmail.com>
2022-03-24 11:57:54 +0100

    Add

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,9 @@ import { FaTwitterSquare } from "react-icons/fa";
 function Home() {
     return (
         <div className="container1">
+            
+            {/* Left  */}
+
             <div className='left'>
                 <div className='box11'> 
                 <div className='box1'>
@@ -31,18 +34,60 @@ function Home() {
               
                         <a href='#'>افلام بلوراى</a>
                     </div>
-
-
                     <div className='link2'>
                         <a href='#'>افلام مصرية</a>
                 
                         <a href='#'>افلام كورية</a>
                     </div>
 
+                    <div className='link2'>
+                        <a href='#'>افلام وثائقية</a>
+                
+                        <a href='#'>افلام حرب</a>
+                    </div>
 
-                  
+
+                    <div className='link2'>
+                        <a href='#'>افلام آسيوية</a>
+                
+                        <a href='#'>افلام رعب</a>
+                    </div>
+
+
+                    <div className='link2'>
+                        <a href='#'>افلام اكشن</a>
+                
+                        <a href='#'>افلام رومانسية</a>
+                    </div>
+
+
+                    <div className='link2'>
+                        <a href='#'>افلام كرتون</a>
+                
+                        <a href='#'>ايجي بست</a>
+                    </div>
+
+
+                    <div className='link2'>
+                        <a href='#'>افلام خيال علمى</a>
+                
+                        <a href='#'>افلام كورية</a>
+                    </div>
+
+
+                    <div className='link2'>
+                        <a href='#'>افلام مصرية</a>
+                
+                        <a href='#'>افلام وثائقية</a>
+                    </div>
                 </div>
             </div>
+            </div>
+
+            <div className='center'>
+                <div className='box3'>
+                <h1>مرحباً بكم في موقع ايجي بست الاصلي</h1>
+                </div>
             </div>
         </div>
     )
