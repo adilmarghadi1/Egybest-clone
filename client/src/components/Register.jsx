b650b530dc52dfff6da6cf13db3f42eb0422821b
adilmarghadi1 <adilmarghadi2020@gmail.com>
2022-03-25 09:48:32 +0100

    Add

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,10 +1,19 @@
 import React from "react"
-
+import { AiOutlineGooglePlus } from "react-icons/ai";
+import { AiFillFacebook } from "react-icons/ai";
+import './Register.css'
 function Login() {
     return (
         
-        <div>
-            
+        <div className="container2">
+            <div className="box9">
+
+            </div>
+
+            <div className="box10">
+                <a href="#"><AiFillFacebook /> Sign in with Facebook</a>
+                <a href="#"><AiOutlineGooglePlus />Sign In With Google</a>
+            </div>
         </div>
     )
 }
