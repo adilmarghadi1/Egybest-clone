6ba17ebb1880e273253e6caccd9dc79e7fb94ce0
adilmarghadi1 <adilmarghadi2020@gmail.com>
2022-03-25 17:58:51 +0100

    Add

--- backend/index.js
+++ backend/index.js
@@ -8,3 +8,15 @@ app.use(cors())
 app.use(express.json())
 
 require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI,
+    err => {
+        if(err) throw err
+
+        console.log("Connected Successfully to MongoDB")
+    });
+
+
+    app.listen(PORT, () => {
+        console.log(`server are run on ${PORT}`)
+    })
