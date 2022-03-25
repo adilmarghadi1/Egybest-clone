e53cb0c31a9a89b332b44b520fa4ab8718df3080
adilmarghadi1 <adilmarghadi2020@gmail.com>
2022-03-25 14:30:32 +0100

    Add

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -13,7 +13,6 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-// import required modules
 import { Pagination, Navigation } from "swiper";
 
 function Home() {
